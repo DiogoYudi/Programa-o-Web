145f451a7a8e5c4c936fd97eba6b3b3d02137c86
DiogoYudi <130068159+DiogoYudi@users.noreply.github.com>
2023-08-23 09:20:55 -0300

    Add files via upload

--- Atividade 1.1 Prog.Web/Exercicio5/index.php
+++ Atividade 1.1 Prog.Web/Exercicio5/index.php
@@ -15,9 +15,36 @@
         <input type="number" name="inputNum3" placeholder="Parcela">
         <input type="submit" value="Calcular">
     </form>
-    <?php
-        if(isset($_SESSION['Resultado']))
-            echo "Multa = " . $_SESSION['Resultado'];
-    ?>
+<table>
+    <thead>
+        <tr>
+            <th> Parcela </th>
+            <th> Vlr Parcela </th>
+            <th> Amortização </th>
+            <th> Juros </th>
+            <th> Sdo Devedor </th>
+        </tr>
+    </thead>
+    <tbody>
+            <?php
+                if(isset($_SESSION['Resultado'])){
+                    echo "<tr>";
+                    foreach($_SESSION['Resultado'] as $row){
+                        foreach($row as $col){
+                            echo "<td>" . $col . "</td>";
+                        }
+                    echo "</tr>";
+                    }
+                }
+            ?>
+        </tbody>
+    </tbody>
+</table>
+<?php
+    if(isset($_SESSION['juros'])){
+        echo "<br>";
+        echo "Total de juros pago: R$ " . $_SESSION['juros'];
+    }
+?>
 </body>
 </html>
--- Atividade 1.1 Prog.Web/Exercicio5/processamento.php
+++ Atividade 1.1 Prog.Web/Exercicio5/processamento.php
@@ -3,6 +3,7 @@
     
     if(!empty($_POST['inputNum']) && (!empty($_POST['inputNum2'])) && (!empty($_POST['inputNum3'])))
     {
+        $list = [];
         $m = $_POST['inputNum'];
         $j = $_POST['inputNum2'];
         $p = $_POST['inputNum3'];
@@ -11,14 +12,24 @@
         $parcela = $m * ((($j/100) * ((1+($j/100))**$p))/(((1+($j/100))**$p)-1));
         $parcela = round($parcela, 2);
         for ($i = 0; $i < $p; $i++){
+            $list2 = [];
+            $a = "N° " . $i+1;
             $juros = round(($sdo_devedor * $j)/100, 2);
             $amortizacao = round($parcela - $juros, 2);
             $sdo_devedor = round($sdo_devedor - $amortizacao, 2);
-            $totaljuros += $juros; 
+            $totaljuros += $juros;
             if($sdo_devedor < 0)
                 $sdo_devedor = 0;
-            echo "Parcela: " . $parcela . "....\n" . "Amortização: " . $amortizacao . "....\n" . "Juros: " . $juros . "....\n" . "Saldo Devedor: " . $sdo_devedor . "<br>";
+            array_push($list2, $a);
+            array_push($list2, $parcela);
+            array_push($list2, $amortizacao);
+            array_push($list2, $juros);
+            array_push($list2, $sdo_devedor);
+            array_push($list, $list2);
         }
-        echo "Total Juros: " . $totaljuros;
+        $_SESSION['Resultado'] = $list;
+        $_SESSION['juros'] = $totaljuros;
+        header('location:../Exercicio5/index.php');
+        die();
     }
 ?>
